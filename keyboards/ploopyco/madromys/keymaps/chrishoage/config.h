198c730711d755cd503748094004868ed8c26443
Chris Hoage <iam@chrishoage.com>
2024-08-04 13:41:47 -0700

    Migrate custom keymaps

---
+++ keyboards/ploopyco/madromys/keymaps/chrishoage/config.h
@@ -0,0 +1,10 @@
+// Copyright 2022 Chris Hoage (@chrishoage)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define PLOOPY_DRAGSCROLL_INVERT
+#define PLOOPY_DRAGSCROLL_DIVISOR_V 15.0
+#define PLOOPY_DPI_DEFAULT 1
+#define PLOOPY_DPI_OPTIONS \
+        { 300, 400, 500, 600 }
---
+++ keyboards/spleeb/keymaps/chrishoage/config.h
@@ -0,0 +1,14 @@
+// Copyright 2022 Chris Hoage (@chrishoage)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define AUTO_MOUSE_TIME 250
+
+#define CIRQUE_PINNACLE_DIAMETER_MM 35
+
+#define SPLEEB_DRAGSCROLL_REVERSE_X
+#define SPLEEB_ENCODER_MODE_MAP_ENABLE
+
+#define BOOTMAGIC_ROW 5
+#define BOOTMAGIC_COLUMN 6
